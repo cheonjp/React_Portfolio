975e6106570385b2aab5fc519695c847629d8fda
Jeongpil <nmmcjp@gmail.com>
2021-11-11 17:55:39 -0800

    making menu data

---
+++ 4.menu/src/App.js
@@ -0,0 +1,13 @@
+import React, {useState} from 'react'
+import menu from './menu.js'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ 4.menu/src/menu.js
@@ -0,0 +1,101 @@
+export default [
+    {
+    id:1,
+    category:'main',
+    menu:'grilled Beef',
+    price: 35,
+    img:'\img\beef_grilled.jpg',
+    explain:'The high heat of grilling sears the surface of beef, creating tender meat with a flavorful crust. The required cooking temperature and the method of grilling (direct, indirect, or a combination) depends on the cut of beef and the quality of the meat.'
+},
+{
+    id:2,
+    category:'main',
+    menu:'Beef Steak',
+    price:38,
+    img:'\img\beef_steak.jpg',
+    explain:'A beefsteak, often called just steak, is a flat cut of beef with parallel faces, usually cut perpendicular to the muscle fibers.'
+},
+{
+    id:3,
+    category:'appetizer',
+    menu:'English Muffin',
+    price:13,
+    img:'\img\breakfast_bread.jpg',
+    explain:'An English muffin is a small, round and flat yeast-leavened sourdough bread which is commonly sliced horizontally, toasted, and buttered.'
+},
+{
+    id:4,
+    category:'desert',
+    menu:'Cafe Latte',
+    price:6,
+    img:'4.menu\src\img\cafelatte.jpg',
+    explain:'Caffè latte, often shortened to just latte in English, is a coffee drink of Italian origin made with espresso and steamed milk.'
+},
+{
+    id:5,
+    category:'appetizer',
+    menu:'Sandwich',
+    price:8,
+    img:'\img\cheese_sandwiches.jpg',
+    explain:'Spread 2 tablespoons herb mayo on 4 slices of bread. Spread 1 tablespoon mustard on the other 4 slices of bread. Top each mayo slice of bread with 3 slices of ham, 2 slices of provolone, arugula, a couple slices of tomato, and red onion.'
+}
+,
+{
+    id:6,
+    category:'desert',
+    menu:'Green Tea',
+    price:7,
+    img:'\img\green_tea.jpg',
+    explain:'Green tea is a type of tea that is made from Camellia sinensis leaves and buds that have not undergone the same withering and oxidation process used to make oolong teas and black teas.'
+},
+{
+    id:7,
+    category:'main',
+    menu:'Hamburger',
+    price:13,
+    img:'\img\hambergur.jpg',
+    explain:'A hamburger is a food, typically considered a sandwich, consisting of one or more cooked patties—usually ground meat, typically beef—placed inside a sliced bread roll or bun.'
+},
+{
+    id:8,
+    category:'desert',
+    price:7,
+    menu:'Herbal Tea',
+    img:'\img\herb_tea.jpg',
+    explain:'Herbal teas also known as herbal infusions—and less commonly called tisanes —are beverages made from the infusion or decoction of herbs, spices, or other plant material in hot water.'
+},
+{
+    id:9,
+    category:'desert',
+    price:9,
+    menu:'Pancake',
+    img:'\img\pancake.jpg',
+    explain:'A pancake is a flat cake, often thin and round, prepared from a starch-based batter that may contain eggs, milk and butter and cooked on a hot surface such as a griddle or frying pan, often frying with oil or butter.'
+},
+{
+    id:10,
+    category:'appetizer',
+    menu:'Dynamite Roll',
+    price:6,
+    img:'\img\sushi_roll.jpg',
+    explain:'A dynamite roll is a type of Western-style sushi. It usually contains a piece of prawn tempura and masago (capelin roe), with vegetables like radish sprouts, avocado and/or cucumber, as well as Japanese mayonnaise. In another variant, hamachi (yellowtail) replaces prawn tempura.'
+},
+{
+    id:11,
+    category:'main',
+    menu:'Sweet Sour Chicken',
+    price:18,
+    img:'\img\sweet_sour_chiken.jpg',
+    explain:'The dish consists of deep fried pork or chicken with corn flour batter into bite-sized pieces and subsequently coated with the sweet and sour sauce that consists of ketchup, chili sauce, plum sauce, sugar, lemon juice and salt.'
+},
+{
+    id:12,
+    category:'desert',
+    menu:'Turkish Coffee',
+    price:7,
+    img:'\img\turkish_coffee.jpg',
+    explain:'Turkish coffee is very finely ground coffee brewed by boiling. Any coffee bean may be used; arabica varieties are considered best.'
+}
+
+
+]
