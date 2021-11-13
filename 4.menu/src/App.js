6c8c3e99411d5e4c2a5db21493064089f42b4df0
Jeongpil <nmmcjp@gmail.com>
2021-11-13 01:37:44 -0800

    creating no.4

--- 4.menu/src/App.js
+++ 4.menu/src/App.js
@@ -1,11 +1,29 @@
 import React, {useState} from 'react'
-import menu from './menu.js'
+import Item from './Item'
+import menu from './menu'
+import Categories from './Categories'
 import './App.css';
 
 function App() {
+  const [foot, setFoot]=useState(menu)
+
+  const filterMenu = (category)=>{
+    
+    // const newItems = menu.filter((item)=>item.category === category)
+    const newItems = menu.filter((item)=>{
+      return item.category === category
+      })
+      setFoot(newItems)
+  }
+
   return (
     <div className="App">
-      
+      <h1>Menu</h1>
+      <div className="line"></div>
+        <button onClick={()=>filterMenu('appetizer')}>breakfast</button>
+      <div className="container">
+        <Item footMenu ={foot}/>
+      </div>
     </div>
   );
 }
--- 4.menu/src/menu.js
+++ 4.menu/src/menu.js
@@ -1,10 +1,22 @@
+import grilledBeef from './img/beef_grilled.jpg'
+import beefSteak from './img/beef_steak.jpg'
+import breakfastBread from './img/breakfast_bread.jpg'
+import cafeLatte from './img/cafelatte.jpg'
+import cheeseSandwich from './img/cheese_sandwiches.jpg'
+import greenTea from './img/green_tea.jpg'
+import hamburger from './img/hambergur.jpg'
+import herbarTea from './img/herb_tea.jpg'
+import pancake from './img/pancake.jpg'
+import sushiRoll from './img/sushi_roll.jpg'
+import sweetChicken from './img/sweet_sour_chiken.jpg'
+import turkishCoffee from './img/turkish_coffee.jpg'
 export default [
     {
     id:1,
     category:'main',
     menu:'grilled Beef',
     price: 35,
-    img:'\img\beef_grilled.jpg',
+    img: grilledBeef,
     explain:'The high heat of grilling sears the surface of beef, creating tender meat with a flavorful crust. The required cooking temperature and the method of grilling (direct, indirect, or a combination) depends on the cut of beef and the quality of the meat.'
 },
 {
@@ -12,7 +24,7 @@ export default [
     category:'main',
     menu:'Beef Steak',
     price:38,
-    img:'\img\beef_steak.jpg',
+    img:beefSteak,
     explain:'A beefsteak, often called just steak, is a flat cut of beef with parallel faces, usually cut perpendicular to the muscle fibers.'
 },
 {
@@ -20,7 +32,7 @@ export default [
     category:'appetizer',
     menu:'English Muffin',
     price:13,
-    img:'\img\breakfast_bread.jpg',
+    img:breakfastBread,
     explain:'An English muffin is a small, round and flat yeast-leavened sourdough bread which is commonly sliced horizontally, toasted, and buttered.'
 },
 {
@@ -28,7 +40,7 @@ export default [
     category:'desert',
     menu:'Cafe Latte',
     price:6,
-    img:'4.menu\src\img\cafelatte.jpg',
+    img:cafeLatte,
     explain:'Caffè latte, often shortened to just latte in English, is a coffee drink of Italian origin made with espresso and steamed milk.'
 },
 {
@@ -36,7 +48,7 @@ export default [
     category:'appetizer',
     menu:'Sandwich',
     price:8,
-    img:'\img\cheese_sandwiches.jpg',
+    img: cheeseSandwich,
     explain:'Spread 2 tablespoons herb mayo on 4 slices of bread. Spread 1 tablespoon mustard on the other 4 slices of bread. Top each mayo slice of bread with 3 slices of ham, 2 slices of provolone, arugula, a couple slices of tomato, and red onion.'
 }
 ,
@@ -45,7 +57,7 @@ export default [
     category:'desert',
     menu:'Green Tea',
     price:7,
-    img:'\img\green_tea.jpg',
+    img:greenTea,
     explain:'Green tea is a type of tea that is made from Camellia sinensis leaves and buds that have not undergone the same withering and oxidation process used to make oolong teas and black teas.'
 },
 {
@@ -53,7 +65,7 @@ export default [
     category:'main',
     menu:'Hamburger',
     price:13,
-    img:'\img\hambergur.jpg',
+    img:hamburger,
     explain:'A hamburger is a food, typically considered a sandwich, consisting of one or more cooked patties—usually ground meat, typically beef—placed inside a sliced bread roll or bun.'
 },
 {
@@ -61,7 +73,7 @@ export default [
     category:'desert',
     price:7,
     menu:'Herbal Tea',
-    img:'\img\herb_tea.jpg',
+    img:herbarTea,
     explain:'Herbal teas also known as herbal infusions—and less commonly called tisanes —are beverages made from the infusion or decoction of herbs, spices, or other plant material in hot water.'
 },
 {
@@ -69,7 +81,7 @@ export default [
     category:'desert',
     price:9,
     menu:'Pancake',
-    img:'\img\pancake.jpg',
+    img:pancake,
     explain:'A pancake is a flat cake, often thin and round, prepared from a starch-based batter that may contain eggs, milk and butter and cooked on a hot surface such as a griddle or frying pan, often frying with oil or butter.'
 },
 {
@@ -77,7 +89,7 @@ export default [
     category:'appetizer',
     menu:'Dynamite Roll',
     price:6,
-    img:'\img\sushi_roll.jpg',
+    img:sushiRoll,
     explain:'A dynamite roll is a type of Western-style sushi. It usually contains a piece of prawn tempura and masago (capelin roe), with vegetables like radish sprouts, avocado and/or cucumber, as well as Japanese mayonnaise. In another variant, hamachi (yellowtail) replaces prawn tempura.'
 },
 {
@@ -85,7 +97,7 @@ export default [
     category:'main',
     menu:'Sweet Sour Chicken',
     price:18,
-    img:'\img\sweet_sour_chiken.jpg',
+    img: sweetChicken,
     explain:'The dish consists of deep fried pork or chicken with corn flour batter into bite-sized pieces and subsequently coated with the sweet and sour sauce that consists of ketchup, chili sauce, plum sauce, sugar, lemon juice and salt.'
 },
 {
@@ -93,7 +105,7 @@ export default [
     category:'desert',
     menu:'Turkish Coffee',
     price:7,
-    img:'\img\turkish_coffee.jpg',
+    img:turkishCoffee,
     explain:'Turkish coffee is very finely ground coffee brewed by boiling. Any coffee bean may be used; arabica varieties are considered best.'
 }
 
